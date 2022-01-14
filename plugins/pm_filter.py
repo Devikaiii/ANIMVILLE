b712755fdce7de70a2fc764cd0a9816fffb83336
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-01-14 10:21:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -433,14 +433,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
-        ]]
+        InlineKeyboardButton('🎭GROUP🎭', url='http://t.me/CK_linkz'),
+        InlineKeyboardButton('🔊CHANNEL🔊', url='https://t.me/EvaMariaUpdates')
+    ],[
+        InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+        InlineKeyboardButton('😊 About', callback_data='about')
+    ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
